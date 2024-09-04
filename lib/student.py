4ac1ef4b4b7d1c0d24697a90e4ed8f8e746858a0
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-09-04 15:09:56 +0300

    Completed the lab

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,32 @@
 class Student:
-    pass
+    # def __init__(self, name):
+    #     self.name = name
+        
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")
+
+        
+    # pass
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        for x in range(0,10):
+            super().raise_hand()
+        
+
+    # pass
+
+
+# Student = Student("Student")
+# Student.hello()
+# Student.raise_hand()
+# ChattyStudent = ChattyStudent("ChattyStudent")
+# ChattyStudent.hello()
+# ChattyStudent.raise_hand()
